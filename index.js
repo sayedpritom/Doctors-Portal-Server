25f64e2eac4c1742c7da562fee4d32373588bfa3
unknown <sayedpritom999@gmail.com>
2022-05-18 04:20:45 +0600

    setup server, created get api to get all data from server

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3el01.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello From Doctors Server!')
+})
+
+app.listen(port, () => {
+  console.log(`Doctors App listening on port ${port}`)
+})
